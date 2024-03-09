6067685016776a1bc45b6479d4e9e70f79eaa860
totom3ndez <tomii.mendez45@gmail.com>
2024-03-09 19:19:00 +0100

    First commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			primary: '#91354e',
+			secondary: '#25282b'
+		},
+		extend: {}
+	},
+	plugins: []
+}
