04c131e3327e4c1c87440da8e7b1c4a49d1fc2b1
totom3ndez <tomii.mendez45@gmail.com>
2024-03-15 18:37:34 +0100

    Upgraded SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-import compress from "astro-compress";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()]
-});
+	integrations: [tailwind()]
+})
