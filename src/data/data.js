c57fa1724235d777f789d92d2e1a3d15f51d97c8
totom3ndez <tomii.mendez45@gmail.com>
2024-03-20 18:29:41 +0100

    Styled services and add data.js for optimization

---
+++ src/data/data.js
@@ -0,0 +1,48 @@
+// MENU LINKS
+
+export const navLinks = [
+	{
+		name: 'Inicio',
+		href: '/#hero'
+	},
+	{
+		name: 'Blog',
+		href: '/blog'
+	},
+	{
+		name: 'Servicios',
+		href: '/#services'
+	},
+	{
+		name: 'Nosotros',
+		href: '/about'
+	},
+	{
+		name: 'Contacto',
+		href: '/#contact'
+	}
+]
+
+// SERVICES
+
+export const services = [
+	{
+		name: 'Asesoría',
+		description:
+			'Te brindamos asesoría en cuanto a armado de carpetas, procedimientos y puntos importantes del tramite de la ciudadanía.',
+		price: 'Consultar precio'
+	},
+	{
+		name: 'Traducciones',
+		description:
+			'Te brindamos asesoría en cuanto a armado de carpetas, procedimientos y puntos importantes del tramite de la ciudadanía.',
+		price: 'Consultar precio'
+	},
+	{
+		name: 'Arbologico',
+		description:
+			'Esta herramienta creada 100% por nosotros te ayuda a crear tu árbol genealogico y asi tener en claro tus antepasados.',
+		price: 'Ingresar a la APP',
+		hrefButton: 'https://totom3ndez.github.io/Arbologico/'
+	}
+]
