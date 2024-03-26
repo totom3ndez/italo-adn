ed53fc84533338e3ef21d7f59ac6a3ec268f7961
Tomas Mendez <109675347+totom3ndez@users.noreply.github.com>
2024-03-26 16:33:55 +0000

    Added SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import netlify from '@astrojs/netlify/functions';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  adapter: netlify(),
+  output: 'server',
 });
