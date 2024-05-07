656b7def55002bb4b15cf363b531c19f61df1e2b
totom3ndez <tomii.mendez45@gmail.com>
2024-05-07 12:45:25 +0200

    updated workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 
+import netlify from '@astrojs/netlify'
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), react()],
 	output: 'server',
+	adapter: netlify(),
 	site: 'https://italoadn.com'
 })
