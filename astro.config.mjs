f09766d49eb78254c0660799b65cb0e9440cb95b
totom3ndez <tomii.mendez45@gmail.com>
2024-05-07 15:07:18 +0200

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
-
 import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), react()],
 	output: 'server',
+	site: 'https://totom3ndez.github.io',
 	adapter: netlify()
 })
