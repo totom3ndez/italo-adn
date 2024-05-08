4e0518da2f4a47a79de3dd040c8da25fb2fecfd2
totom3ndez <tomii.mendez45@gmail.com>
2024-05-08 13:30:52 +0200

    update v2

--- src/components/services/Asesoria.jsx
+++ src/components/services/Asesoria.jsx
@@ -26,8 +26,8 @@ export default function Asesoria() {
 					'Content-Type': 'application/json'
 				},
 				body: JSON.stringify({
-					to: 'italoadn.ok@gmail.com',
-					from: 'italoadn.ok@gmail.com',
+					to: 'info@italoadn.com',
+					from: 'info@italoadn.com',
 					subject: `Nueva asesoría para ${name}`,
 					html: `
 						<h1>Formulario de Asesoría</h1>
