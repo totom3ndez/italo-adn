1fb477a8b7357ae6ba33892999b9713200599bc5
totom3ndez <tomii.mendez45@gmail.com>
2025-01-31 17:31:24 +0100

    Edited texts and footer

--- src/data/data.js
+++ src/data/data.js
@@ -31,21 +31,21 @@ export const services = [
 	{
 		name: 'Asesorías',
 		description:
-			'Te guío en el proceso, y ayudo a responder tus dudas. Podemos ajustarlo al horario que más te convenga, ya que es Online.',
+			'Te guío en el proceso de obtención de la ciudadanía italiana y respondo todas tus dudas. Podemos ajustar las asesorías al horario que más te convenga, ya que todo es online.',
 		button: 'Ver más',
 		hrefButton: '/servicios/asesoria'
 	},
 	{
 		name: 'Traducciones',
 		description:
-			'Para iniciar el proceso de solicitud de Ciudadanía Italiana en Italia, es imprescindible realizar la traducción de los registros que conforman tu carpeta y que no sean de origen italiano.',
+			'Para iniciar el proceso de solicitud de la Ciudadanía Italiana en Italia, es imprescindible traducir los registros que conforman tu expediente y que no sean de origen italiano. Nos encargamos de realizar traducciones certificadas para tu carpeta.',
 		button: 'Ver más',
 		hrefButton: '/servicios/traducciones'
 	},
 	{
 		name: 'Solicitud de actas',
 		description:
-			'El primer paso para obtener tu ciudadanía es ubicar los registros de tu antepasado, ¡y	nosotros nos encargamos de eso por vos!',
+			'El primer paso para obtener tu ciudadanía italiana es ubicar los registros de tu antepasado. ¡Nosotros nos encargamos de buscar y solicitar las actas necesarias por ti!',
 		button: 'Ver más',
 		hrefButton: '/servicios/actas-italianas'
 	}
